b4b068740f68938344b4abe17270c18e7503144a
Borys Rybak <boryb@microsoft.com>
2018-03-20 19:18:52 +0100

    Add configuration code.

--- RealTimeCrowdInsights/Bootstrapper.cs
+++ RealTimeCrowdInsights/Bootstrapper.cs
@@ -1,19 +1,47 @@
 ﻿using Caliburn.Micro;
 using RealTimeCrowdInsights.ViewModels;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace RealTimeCrowdInsights
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container;
+        
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _container = new SimpleContainer();
+
+            _container.PerRequest<ShellViewModel>();
+
+            IoC.GetInstance = GetInstance;
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
