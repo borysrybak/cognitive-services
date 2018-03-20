50dec33e4a7e88b486cfe39b11a5b54d74f6b6a8
Borys Rybak <boryb@microsoft.com>
2018-03-20 16:32:06 +0100

    Configure Bootstrapper startup.

---
+++ RealTimeCrowdInsights/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using RealTimeCrowdInsights.ViewModels;
+using System.Windows;
+
+namespace RealTimeCrowdInsights
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
